39691d5654983bed9644beff0da8bb88c769d15f
seanhersee <cjf4xv@virginia.edu>
2026-05-23 17:11:09 +0000

    saving two init files

---
+++ init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER=cjf4xv@virginia.edu
+NAME=seanhersee
+
+git config --global --list
+
+git config --global user.email ${USER}
+git config --global user.name  ${NAME}
+
+git config --global --list
