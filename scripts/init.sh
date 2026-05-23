306a117fe5121994b4b52072fbffd9fdaaeff085
seanhersee <cjf4xv@virginia.edu>
2026-05-23 17:43:43 +0000

    added makefile and requirements

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,5 @@
 sudo apt update
 sudo apt install make -y
-sudo apt install python3.12-venv -y
-sudo apt install tree
+sudo apt install python3.14 -y
+sudo apt install python3.14-venv -y
+sudo apt install tree -y
