80d6370ca18337e5e2d5c24856e00710913d3a79
seanhersee <cjf4xv@virginia.edu>
2026-06-06 12:46:27 +0000

    Changes to address review comments. Funcions created to wrap scripts in main()

--- scripts/clean_ids.py
+++ scripts/clean_ids.py
@@ -6,14 +6,21 @@ import re
 logging.basicConfig(filename = 'pipeline_autid.log', filemode = 'a', format = '%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-data = sys.stdin
-regex_pattern = '^[A-Za-z0-9_-]{11}$'
 
-for line in data:
-    line = line.strip()
-    if not line:
-        continue
-    if re.fullmatch( regex_pattern, line):
-        sys.stdout.write(line + '\n')
-    else:
-        logger.error(f'{line} is not a valid id')
+def is_valid_youtube_id(id):
+	regex_pattern = '^[A-Za-z0-9_-]{11}$'
+	return bool(re.fullmatch(regex_pattern, id))
+
+
+def main():
+	for line in sys.stdin:
+    		line = line.strip()
+    		if not line:
+        		continue
+    		if is_valid_youtube_id(line):
+        		sys.stdout.write(line + '\n')
+    		else:
+        		logger.error(f'{line} is not a valid id')
+
+if __name__ =="__main__":
+	main()
