b815a5f8a1eb7192d7b22ffce5d492c385b3750a
seanhersee <cjf4xv@virginia.edu>
2026-07-30 23:18:13 +0000

    Removed unnecessary files

--- models/orchestrate_pipeline.sql
+++
@@ -1,4 +0,0 @@
--- Master Pipeline Orchestrator
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/scripts/transform/01_stg_youtube_transcripts.sql;
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/scripts/transform/02_dim_videos.sql;
-EXECUTE IMMEDIATE FROM @DS5111_GIT_STAGE/branches/LAB09_gitops_snowflake/scripts/transform/03_fct_entities.sql;
